9e5f3f2d979e8253e4d10d7022a17a35f878cf87
sarth-shr <126249666+sarth-shr@users.noreply.github.com>
2023-03-13 12:23:14 +0545

    Bootstrap Packer Installation

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,3 +1,20 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer...")
+	vim.cmd([[packadd packer.nvim]])
+end
+
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd([[
   augroup packer_user_config
@@ -88,4 +105,10 @@ return packer.startup(function(use)
 
 	-- Discord Rich Presence
 	use("andweeb/presence.nvim")
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
