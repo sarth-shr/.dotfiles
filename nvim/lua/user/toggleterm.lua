ebc5154c5eb4bfa31bd4c78c320b134cfd3fe305
sarth-shr <126249666+sarth-shr@users.noreply.github.com>
2023-03-31 15:09:28 +0545

    Keybind updates

--- nvim/lua/user/toggleterm.lua
+++ nvim/lua/user/toggleterm.lua
@@ -6,7 +6,7 @@ if not status_ok then
 end
 
 toggleterm.setup({
-	size = 20,
+	size = 15,
 	open_mapping = [[<c-\>]],
 	shade_terminals = true,
 	shading_factor = 2,
@@ -23,8 +23,7 @@ toggleterm.setup({
 
 function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
-	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+	-- vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
 	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
 	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
 	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
