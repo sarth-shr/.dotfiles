69641818dcb5d341797b09eeb65a0eecad96c098
sarth-shr <126249666+sarth-shr@users.noreply.github.com>
2023-12-08 12:40:26 +0545

    Migrated to version 3

--- nvim/lua/user/indentline.lua
+++ nvim/lua/user/indentline.lua
@@ -1,15 +1,7 @@
 -- Protected call to prevent config crashing
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, ibl = pcall(require, "ibl")
 if not status_ok then
-  vim.notify("Indent Blankline failed to load!")
+	vim.notify("Indent Blankline failed to load!")
 end
 
--- vim.opt.list = true
--- vim.opt.listchars:append "space:⋅"
--- vim.opt.listchars:append "eol:↴"
-
-indent_blankline.setup {
-  space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = true,
-}
+ibl.setup({})
